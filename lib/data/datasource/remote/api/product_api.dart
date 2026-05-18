9e004a746c6c6b4d0768616b77fc48f1c810e716
Sajedul Islam <sajedul.dev@gmail.com>
2026-05-18 23:32:15 +0600

    refactor(network): clean up product_api interface and implementation

--- lib/data/datasource/remote/api/product_api.dart
+++ lib/data/datasource/remote/api/product_api.dart
@@ -1,9 +1,5 @@
 import 'package:productify/domain/model/product/product.dart';
 
 abstract class ProductApi {
-  Future<List<Product>> getAllProducts({
-    required int page,
-    required int limit,
-});
+  Future<List<Product>> getAllProducts({required int page, required int limit});
 }
-
--- lib/data/datasource/remote/api/product_api_impl.dart
+++ lib/data/datasource/remote/api/product_api_impl.dart
@@ -1,4 +1,3 @@
-
 import 'package:productify/data/datasource/remote/api/product_api.dart';
 import 'package:productify/data/datasource/remote/model/response/product/product_response.dart';
 import 'package:productify/data/datasource/remote/util/api_client.dart';
@@ -20,13 +19,13 @@ class ProductApiImpl extends ProductApi {
     try {
       final response = await _client.get<JSONList, List<ProductResponse>>(
         path: getAllProductsUrl,
-        queryParameters: {
-          'page': page.toString(),
-          'limit': limit.toString(),
-        },
+        queryParameters: {'page': page.toString(), 'limit': limit.toString()},
         converter: (jsonList) {
           return jsonList
-              .map((json) => ProductResponse.fromJson(json as Map<String, dynamic>))
+              .map(
+                (json) =>
+                    ProductResponse.fromJson(json as Map<String, dynamic>),
+              )
               .toList();
         },
       );
