cc91d8a98b6532836f23e5f40f5d68b400c31e5a
Sajedul Islam <sajedul.dev@gmail.com>
2026-05-18 23:32:15 +0600

    refactor(network): update RatingResponse model with custom fromJson parsing

--- lib/data/datasource/remote/model/response/rating/rating_response.dart
+++ lib/data/datasource/remote/model/response/rating/rating_response.dart
@@ -1,14 +1,13 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'rating_response.freezed.dart';
+
 part 'rating_response.g.dart';
 
 @freezed
-abstract class RatingResponse with _$RatingResponse{
-  const factory RatingResponse({
-    required double rate,
-    required int count,
-  }) = _RatingResponse;
+abstract class RatingResponse with _$RatingResponse {
+  const factory RatingResponse({required double rate, required int count}) =
+      _RatingResponse;
 
   factory RatingResponse.fromJson(Map<String, dynamic> json) =>
       _$RatingResponseFromJson(json);
