016c196fdd338f9e6bd6b059903b04f784c4d7e6
Sajedul Islam <sajedul.dev@gmail.com>
2026-05-18 23:32:16 +0600

    refactor(onboarding): extract Ring decoration widget from slide illustration

---
+++ lib/presentation/screen/onboarding/widget/ring.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class Ring extends StatelessWidget {
+  const Ring({
+    super.key,
+    required this.size,
+    required this.opacity,
+    required this.color,
+  });
+
+  final double size;
+  final double opacity;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        shape: BoxShape.circle,
+        border: Border.all(color: color.withOpacity(opacity), width: 1.5),
+      ),
+    );
+  }
+}
