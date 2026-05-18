3edc2975a9a383f53ea8c8a9e147c38e56beca11
Sajedul Islam <sajedul.dev@gmail.com>
2026-05-18 23:32:16 +0600

    feat(home): adjust product pagination state to support fixed page limit changes

--- lib/presentation/screen/home/state/product_ui_state.dart
+++ lib/presentation/screen/home/state/product_ui_state.dart
@@ -1,4 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:productify/domain/enum/product/product_sort.dart';
 import 'package:productify/domain/model/product/product.dart';
 
 part 'product_ui_state.freezed.dart';
@@ -9,8 +10,13 @@ class ProductUiState with _$ProductUiState {
 
   const factory ProductUiState.loading() = LoadingState;
 
-  const factory ProductUiState.success({required List<Product> products, @Default(true) bool hasMore}) = SuccessState;
-
+  const factory ProductUiState.success({
+    required List<Product> products,
+    required List<Product> filtered,
+    @Default(ProductSort.none) ProductSort sort,
+    @Default(true) bool hasMore,
+    @Default(false) bool isLoadingMore,
+  }) = SuccessState;
 
   const factory ProductUiState.error(String message) = ErrorState;
 }
